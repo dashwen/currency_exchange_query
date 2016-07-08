4b55f157894dee3d5caae6cab274cf7fffb4fca4
Deshawn Dana <dashwen@gmail.com>
2016-07-08 14:36:46 -0700

    submitting yql currency converter, favorites list not working, and only fetches synchronously

---
+++ currency_exchange_query/ViewController2.h
@@ -0,0 +1,13 @@
+//
+//  ViewController2.h
+//  currency_exchange_query
+//
+//  Created by Deshawn Dana on 3/16/16.
+//  Copyright © 2016 Deshawn Dana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController2 : UIViewController
+
+@end
---
+++ currency_exchange_query/ViewController2.m
@@ -0,0 +1,37 @@
+//
+//  ViewController2.m
+//  currency_exchange_query
+//
+//  Created by Deshawn Dana on 3/16/16.
+//  Copyright © 2016 Deshawn Dana. All rights reserved.
+//
+
+#import "ViewController2.h"
+
+@interface ViewController2 ()
+
+@end
+
+@implementation ViewController2
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
